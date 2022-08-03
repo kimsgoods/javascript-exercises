e0a87f83fd64613cb090532e7b81c12a1485fd94
Kim <kimescabarte@gmail.com>
2022-08-03 09:20:40 +0800

    Answered exercises 1 to 4

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,5 +1,16 @@
-const removeFromArray = function() {
-
+const removeFromArray = function(arr, ...num) {
+    console.log("inital: "+arr); 
+    console.log("remove: "+num);
+    for(let i=arr.length-1; i>=0; i--){
+        for(let j=0; j<num.length; j++){
+            if(arr[i]===num[j]){
+                arr.splice(i,1);
+            }
+        }
+    
+    }
+    console.log("final: "+arr);   
+    return arr;
 };
 
 // Do not edit below this line
