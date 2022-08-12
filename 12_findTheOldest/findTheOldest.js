3ecd23d00759e097330825b8e518f9f9e00bcd13
Kim <kimescabarte@gmail.com>
2022-08-12 10:26:17 +0800

    Answered exercises 9 to 11

--- 12_findTheOldest/findTheOldest.js
+++ 12_findTheOldest/findTheOldest.js
@@ -1,5 +1,9 @@
-const findTheOldest = function() {
-
+const findTheOldest = function(people) {
+       const oldest = inventors.sort(function(a, b) {
+        const lastInventor = a.yearOfDeath - a.yearOfBirth;
+        const nextInventor = b.yearOfDeath - b.yearOfBirth;
+        return lastInventor > nextInventor ? -1 : 1;
+      });
 };
 
 // Do not edit below this line
