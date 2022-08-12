8c3ccc488538cfdcc759f8b211a2c0f3345c3f84
Kim <kimescabarte@gmail.com>
2022-08-12 13:52:36 +0800

    Answered exercises 9 - 12

--- 12_findTheOldest/findTheOldest.js
+++ 12_findTheOldest/findTheOldest.js
@@ -1,10 +1,21 @@
+// let findTheOldest = function (people) {
+//   const age = x => (x.yearOfDeath || new Date().getFullYear()) - x.yearOfBirth;
+
+//   const oldest = people.sort((a, b) =>
+//     age(a) > age(b) ? -1 : 1
+//   );
+//   return oldest[0];
+// };
+
 const findTheOldest = function(people) {
-       const oldest = inventors.sort(function(a, b) {
-        const lastInventor = a.yearOfDeath - a.yearOfBirth;
-        const nextInventor = b.yearOfDeath - b.yearOfBirth;
-        return lastInventor > nextInventor ? -1 : 1;
-      });
-};
+  const thisYear = new Date().getFullYear();
+
+  return people.reduce((res, person) => {
+    const age = (person.yearOfDeath || thisYear) - person.yearOfBirth;
+    return age > res.age ? { person, age } : res;
+  }, { person: null, age: 0 }).person;
+}
+
 
 // Do not edit below this line
 module.exports = findTheOldest;
